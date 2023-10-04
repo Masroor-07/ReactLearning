61d41215481e5b28e7a591f1f80936bef1e97df6
sourav-ojha <subhamojha9@gmail.com>
2023-10-04 12:16:51 +0530

    oct 4

--- my-app/src/NewRoutes.jsx
+++ my-app/src/NewRoutes.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 const MENU = [
+  { id: 0, path: "/login", name: "Login" },
   { id: 1, path: "/", name: "Home" },
   { id: 2, path: "/contact", name: "Contact" },
   { id: 3, path: "/team", name: "Team" },
@@ -9,6 +10,11 @@ const MENU = [
 ];
 
 const NewRoutes = () => {
+  let auth = true;
+
+  const useAuth = (Component) =>
+    auth ? <Component /> : <Navigate to="/login" />;
+
   return (
     <div>
       <div style={{ display: "flex", gap: "5px" }}>
@@ -18,17 +24,22 @@ const NewRoutes = () => {
           </Link>
         ))}
       </div>
+      {/* user logged in -> login hide redirect dashboard/home page */}
+      {auth ? <h2>User Logged in </h2> : <h2>User Not logged in </h2>}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/login" element={auth ? <Navigate to="/" /> : <Login />} />
+        <Route path="/" element={useAuth(Home)} />
+        <Route path="/contact" element={useAuth(Contact)} />
+        <Route path="/team" element={useAuth(Team)} />
+        <Route path="/about" element={useAuth(About)} />
       </Routes>
     </div>
   );
 };
 
 export default NewRoutes;
+
+const Login = () => <div>Login</div>;
 
 const Home = () => (
   <div>
---
+++ routing.md
@@ -0,0 +1,60 @@
+# React-router-dom
+
+## Components
+
+- Navigate - redirect to another path
+- Link - navigate to another path
+
+## hooks
+
+window.location.pathName
+
+useLocation()
+useNavigate()
+
+# API
+
+messgenger - client ----> server and vice versa
+
+REST API - standard api - set of rules for good/standard api
+Representational State Transfer API
+JSON Object
+
+# method - set of rules
+
+GET - get all data or information - no need to send any data
+POST - data write or create - need to send data
+PUT/PATCH - update data - need to send data
+DELETE - delete data - small info
+
+http - not secure
+https - secure with ssl
+
+API url - https://learn.com/api/book/
+method - put / patch
+body - {
+id: 1
+name: sourav
+}
+
+response = result of api
+status code
+data
+message
+errorMessage
+
+# Status Code
+
+2xx - success
+200 - OK
+201 - post - create
+4xx - client error
+400 - bad request
+401 - unauthorized
+403 - forbidden
+404 - Not found
+5xx - server error
+500 - internal server error
+501 - not implemented
+502 - bad gateway
+503 - service unavailable
